33b6efc096ec2e6717a1a10e4eab060ecb6da8fa
indrapradesa <indrapradesa77@gmail.com>
2025-04-10 23:31:58 +0700

    core: workflow code style

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__)
+    ->exclude('vendor');
+
+return (new Config())
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_unused_imports' => true,
+    ])
+    ->setFinder($finder)
+    ->setRiskyAllowed(true);
