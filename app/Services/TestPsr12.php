2ef696bc3894efd16d76909bada74d46b422dcac
indrapradesa <indrapradesa77@gmail.com>
2025-04-11 00:06:47 +0700

    ret

--- app/Services/TestPsr12.php
+++ app/Services/TestPsr12.php
@@ -1,7 +1,15 @@
 <?php
+
 namespace App\Services;
-class   TestPsr12{
-public function   abc( $foo ,$bar ){
-if($foo===$bar){echo "Same";}
-else{echo"Different";}
-}}
+
+class TestPsr12
+{
+    public function abc($foo, $bar)
+    {
+        if ($foo === $bar) {
+            echo "Same";
+        } else {
+            echo"Different";
+        }
+    }
+}
